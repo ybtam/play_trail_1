64e313ebf1e3a08dbaebf5d5220aad112d7ea256
yitam <tam.yi3d@gmail.com>
2019-06-08 18:22:01 +0100

    api hello world

---
+++ app/controllers/ApiController.scala
@@ -0,0 +1,10 @@
+package controllers
+
+import javax.inject.Inject
+import play.api.mvc.{AbstractController, ControllerComponents}
+
+class ApiController @Inject()(cc: ControllerComponents) extends AbstractController(cc){
+  def ping = Action{ implicit  request =>
+    Ok("Hello Scala")
+  }
+}
