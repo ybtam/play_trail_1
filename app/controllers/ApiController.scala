bfd92df591e56f81912520285219931cbb913b4b
yitam <tam.yi3d@gmail.com>
2019-06-08 19:44:31 +0100

    added post and comment retrieve

--- app/controllers/ApiController.scala
+++ app/controllers/ApiController.scala
@@ -1,10 +1,26 @@
 package controllers
 
-import javax.inject.Inject
+import javax.inject.{Inject, Singleton}
+import play.api.libs.json.Json
 import play.api.mvc.{AbstractController, ControllerComponents}
+import repositories.DataRepository
 
-class ApiController @Inject()(cc: ControllerComponents) extends AbstractController(cc){
+@Singleton
+class ApiController @Inject()(
+                               cc: ControllerComponents,
+                               dataRepository: DataRepository
+                             ) extends AbstractController(cc){
   def ping = Action{ implicit  request =>
     Ok("Hello Scala")
+  }
+
+  def getPost(postId: Int) = Action{ implicit request =>
+    dataRepository.getPost(postId) map { post =>
+      Ok(Json.toJson(post))
+    }getOrElse NotFound
+  }
+
+  def getComments(postId: Int) = Action { implicit request =>
+    Ok(Json.toJson(dataRepository.getComments(postId)))
   }
 }
